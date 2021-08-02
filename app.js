d74f64c2d6cadd9d1e48efd58179c8f59585eae5
Andrea Lin <andre93.lin@gmail.com>
2021-08-02 19:31:29 -0300

    put ruleta in a card

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 // Referencia: https://stackoverflow.com/questions/24014075/html5-video-play-pause-video-on-screen-click
+// Hace que se pueda pausar / volver a play el video/gif
 
 let ruleta = document.getElementById("ruleta");
 ruleta.addEventListener("click", function (e) {
